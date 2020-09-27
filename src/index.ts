875d57d997d8de143b021e96a6033fcacd17a8cb
Oliver Steiner <oliver@mollo.ch>
2020-09-27 23:27:04 +0200

    moved code of mainMenu from index.ts to menu.ts

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,23 @@
-
-// Load Settings
-import Settings from './ts/settings'
-import Info from './ts/info'
-
-const settingsPanel = new Settings()
-const info = new Info()
-
 // Load Menu
-
 import Menu from "./ts/menu";
 const menu = new Menu()
 
 
-// Add Listeners
+// Load Settings
+import Settings from './ts/settings'
+const settingsPanel = new Settings()
+
+// Add Listeners for Menu control to settingsPanel
 settingsPanel.elems.checkBoxSlowDownAnimation.addEventListener("change", menu.toggleSlowDownAnimation, false)
 settingsPanel.elems.buttonMenuRestart.addEventListener("click", menu.mainMenuRestart)
 settingsPanel.elems.buttonMenuPlay.addEventListener("click", menu.mainMenuPlay)
 settingsPanel.elems.buttonMenuPause.addEventListener("click", menu.mainMenuPause)
 
+
+// load Info
+import Info from './ts/info'
+const info = new Info()
+
+// load preview
 import Preview from './ts/preview'
 const preview = new Preview()
