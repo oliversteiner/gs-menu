4ff2b044b2ca455dbb483e7a14e21739c5266a4b
Oliver Steiner <oliver@mollo.ch>
2020-09-29 01:36:50 +0200

    refactor setting panel functions

--- src/ts/info.ts
+++ src/ts/info.ts
@@ -1,93 +1,99 @@
 import * as mediaQueries from '../data/MediaQueries.json'
+import Settings from "./settings";
 
 interface MediaQuery {
-  label: string
-  name: string
-  icon: string
-  maxWidth: number
+    label: string
+    name: string
+    icon: string
+    maxWidth: number
 }
 
 interface ScreenSize {
-  x: number
-  y: number
+    x: number
+    y: number
 }
 
 
 class Info {
-  screenSize: ScreenSize
-  mediaQuery: MediaQuery | undefined
-  resizeTimer: any
-  mediaQueries: MediaQuery[]
-
-  constructor() {
-    console.log('Info loaded')
-
-    this.mediaQueries = Object.values(mediaQueries) // Convert Module to Array
-    this.screenSize = {x: 0, y: 0}
-    this.mediaQuery = Info.getMediaQuery()
-
-    this.init()
-  }
+    screenSize: ScreenSize = {x: 0, y: 0}
+    mediaQuery: MediaQuery | undefined
+    resizeTimer: any
+    mediaQueries: MediaQuery[]
+    private name = 'Info';
 
 
-  public static getScreenSize(): { x: number; y: number } {
-    const width = window.innerWidth
-    const height = window.innerHeight
-    return {x: width, y: height}
-  }
+    constructor() {
+        console.log('Info loaded')
 
-
-  public static getMediaQuery(): MediaQuery {
-    const width = window.innerWidth
-
-    // if Screen resolution is bigger then 1200px
-    // return MediaQuery "large"
-    if (width > 1200) {
-      return mediaQueries[3]
+        this.mediaQueries = Object.values(mediaQueries) // Convert Module to Array
+        this.mediaQuery = Info.getMediaQuery()
+        this.buildSettingsSection()
+        this.init()
     }
 
-    // Get MediaQuery with appropriate resolution
-    return mediaQueries.filter(mediaQuery => {
-      console.log(mediaQuery.label + '   - ' + width)
-      if (width <= mediaQuery.maxWidth) {
-        return mediaQuery
-      }
-    })[0]
 
-  }
+    public static getScreenSize(): { x: number; y: number } {
+        const width = window.innerWidth
+        const height = window.innerHeight
+        return {x: width, y: height}
+    }
 
-  private resizeEvent(): void {
-    clearTimeout(this.resizeTimer)
-    this.resizeTimer = setTimeout(() => {
-      const screen = Info.getScreenSize()
-      const media = Info.getMediaQuery()
-      Info.updateSettingsPanel(screen, media)
 
-      this.screenSize = {x: screen.x, y: screen.y}
-      this.mediaQuery = media
-    }, 250)
-  }
+    public static getMediaQuery(): MediaQuery {
+        const width = window.innerWidth
 
-  init(): void {
-    // Get current ScreenInfos
-    this.resizeEvent()
+        // if Screen resolution is bigger then 1200px
+        // return MediaQuery "large"
+        if (width > 1200) {
+            return mediaQueries[3]
+        }
 
-    // Listen to Screen Resizing
-    window.addEventListener("resize", this.resizeEvent)
-  }
+        // Get MediaQuery with appropriate resolution
+        return mediaQueries.filter(mediaQuery => {
+            console.log(mediaQuery.label + '   - ' + width)
+            if (width <= mediaQuery.maxWidth) {
+                return mediaQuery
+            }
+        })[0]
 
-  private static updateSettingsPanel(screen: ScreenSize, media: MediaQuery) {
+    }
 
-    // Get Elements in Dom
-    const containerScreen = document.querySelector('.display-screen-size')
-    const containerMediaQuery = document.querySelector('.display-media-query')
+    private resizeEvent(): void {
+        clearTimeout(this.resizeTimer)
+        this.resizeTimer = setTimeout(() => {
+            const screen = Info.getScreenSize()
+            const media = Info.getMediaQuery()
+            Info.updateSettings(screen, media)
 
-    // Update Elements
-    containerScreen.innerHTML = '<span class="x-label">x</span>' + screen.x + '<span class="y-label">y</span>' + screen.y
-    containerMediaQuery.innerHTML =
-      '<span class="media-icon"><i class="fas '+ media.icon + '"></i></span>'
-      + media.label
-  }
+            this.screenSize = {x: screen.x, y: screen.y}
+            this.mediaQuery = media
+        }, 250)
+    }
+
+    init(): void {
+        // Get current ScreenInfos
+        this.resizeEvent()
+
+        // Listen to Screen Resizing
+        window.addEventListener("resize", this.resizeEvent)
+    }
+
+
+    private  buildSettingsSection() {
+        Settings.addSection(this.name)
+        Settings.addItem(this.name, 'Screen')
+        Settings.addItem(this.name, 'Media')
+    }
+
+    private static updateSettings(screen: ScreenSize, media: MediaQuery) {
+
+        const screenValue = '<span class="x-label">x</span>' + screen.x + '<span class="y-label">y</span>' + screen.y
+        const mediaQueryValue = '<span class="media-icon"><i class="fas ' + media.icon + '"></i></span>'
+            + media.label
+
+        Settings.updateItem('Info','Screen', screenValue)
+        Settings.updateItem('Info','Media', mediaQueryValue)
+    }
 
 }
 
